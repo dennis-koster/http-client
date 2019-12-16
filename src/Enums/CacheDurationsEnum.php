9b1a4b1fc497b1b94049e50fdbf45c61ffd1410f
Dennis Koster <dennis.koster@pxlwidgets.com>
2019-12-16 11:31:52 +0100

    :sparkles: Add more cache duration values

--- src/Enums/CacheDurationsEnum.php
+++ src/Enums/CacheDurationsEnum.php
@@ -4,7 +4,24 @@ namespace DennisKoster\HttpClient\Enums;
 
 use MyCLabs\Enum\Enum;
 
+/**
+ * @method static CacheDurationsEnum DURATION_1_MIN()
+ * @method static CacheDurationsEnum DURATION_5_MIN()
+ * @method static CacheDurationsEnum DURATION_10_MIN()
+ * @method static CacheDurationsEnum DURATION_15_MIN()
+ * @method static CacheDurationsEnum DURATION_30_MIN()
+ * @method static CacheDurationsEnum DURATION_1_HOUR()
+ * @method static CacheDurationsEnum DURATION_1_DAY()
+ * @method static CacheDurationsEnum DURATION_1_WEEK()
+ */
 class CacheDurationsEnum extends Enum
 {
-    public const DURATION_10_MIN = 600;
+    public const DURATION_1_MIN = 60;
+    public const DURATION_5_MIN = 5 * self::DURATION_1_MIN;
+    public const DURATION_10_MIN = 10 * self::DURATION_1_MIN;
+    public const DURATION_15_MIN = 15 * self::DURATION_1_MIN;
+    public const DURATION_30_MIN = 30 * self::DURATION_1_MIN;
+    public const DURATION_1_HOUR = 60 * self::DURATION_1_MIN;
+    public const DURATION_1_DAY = 24 * self::DURATION_1_HOUR;
+    public const DURATION_1_WEEK = 7 * self::DURATION_1_DAY;
 }
